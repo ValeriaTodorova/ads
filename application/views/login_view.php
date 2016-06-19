f8fbb53d339d3c60785c38945577b1756766c2c5
emihaylova <ekaterina_mihaylova@abv.bg>
2016-06-19 13:48:32 +0300

    sync

---
+++ application/views/login_view.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+	<title>Login</title>
+</head>
+<body>
+<form action="verifylogin.php" method="post">
+<p>Username</p>
+<input type="text" name="username" value="">
+<p>Password</p>
+<input type="password" name="password" value="">
+<p>Role</p>
+<input type="hidden" name="role" value="role">
+<input type="submit" name="submit" value="Login">
+</form>
+</body>
+</html>
